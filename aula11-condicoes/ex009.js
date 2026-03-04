57a4ab78f5ebbfa0fa554a8bae91c35ff0ab36da
Flávio Vieira <jsv.flavio@gmail.com>
2026-03-03 21:53:42 -0300

    condições

---
+++ aula11-condicoes/ex009.js
@@ -0,0 +1,7 @@
+//condição composta
+var país = 'Chile'
+    if(país == 'Brasil') {
+        console.log(`Você nasceu no ${país}, portando é Brasileiro`)
+    } else {
+        console.log(`Você não nasceu no Brasil, então é estrangeiro`)
+    }
