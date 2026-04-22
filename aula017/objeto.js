48c2786c277e0d927c02be98321d9714818fcdca
Flávio Vieira <jsv.flavio@gmail.com>
2026-04-22 20:59:25 -0300

    Update objeto.js

--- aula017/objeto.js
+++ aula017/objeto.js
@@ -1,7 +1,16 @@
-let amigo = {
+/* let amigo = {
     nome: 'José',
     sexo: 'M',
     peso: 85.4,
     engordar(p){}
 }
-console.log(amigo)
+console.log(amigo) */
+
+function somar() {
+    var total = 0
+    for(var x = 0; x < 15; x++) {
+        total += x
+    }
+
+    console.log(total)
+}
